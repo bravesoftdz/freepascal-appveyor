a20efff1c5bb5d4d4e129c365e8ce8cb6082c521
Mark K <markfirmware@users.noreply.github.com>
2016-10-07 14:14:48 -0400

    Install fpc

--- install-fpc.cmd
+++ install-fpc.cmd
@@ -1,2 +1,4 @@
 curl -fsSL -o fpc-installer.exe "http://downloads.sourceforge.net/project/freepascal/Win32/3.0.0/fpc-3.0.0.i386-win32.exe?r=&ts=1475862864&use_mirror=pilotfiber"
 fpc-installer.exe /verysilent
+echo installed fpc
+fpc appveyortest.pas
