d1089bbdf7884c0affae7fee12838b5776c5d167
Mark K <markfirmware@users.noreply.github.com>
2016-10-07 14:43:07 -0400

    Build

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-fpc appveyortest.pas
+set PATH=c:\FPC\3.0.0\bin\i386-Win32;%PATH%
+fpc appveyortest.pasfpc appveyortest.pas
--- install-fpc.cmd
+++ install-fpc.cmd
@@ -1,6 +1,3 @@
 curl -fsSL -o fpc-installer.exe "http://downloads.sourceforge.net/project/freepascal/Win32/3.0.0/fpc-3.0.0.i386-win32.exe?r=&ts=1475862864&use_mirror=pilotfiber"
 fpc-installer.exe /verysilent
-echo installed fpc
-dir c:\FPC\3.0.0\bin\i386-win32
-set PATH=c:\FPC\3.0.0\bin\i386-Win32;%PATH%
-fpc appveyortest.pas
+fpc -h
