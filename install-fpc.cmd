656bf1a4340ef89dc466809ca8ae492fa02ba3b8
Mark K <markfirmware@users.noreply.github.com>
2016-10-07 13:56:42 -0400

    Download fpc for appveyor

--- install-fpc.cmd
+++ install-fpc.cmd
@@ -1,1 +1,2 @@
-fpc-installer.exe /verysilent
+wget http://downloads.sourceforge.net/project/freepascal/Win32/3.0.0/fpc-3.0.0.i386-win32.exe?r=&ts=1475862864&use_mirror=pilotfiber
+fpc-3.0.0.i386-win32.exe /verysilent
