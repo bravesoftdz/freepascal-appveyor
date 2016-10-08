2d0f5f6142faa9062c3d611f0c1aa4b4db1f9f7d
markfirmware <markfirmware@users.noreply.github.com>
2016-10-08 16:24:08 -0400

    AppVeyor cache

--- install-fpc.cmd
+++ install-fpc.cmd
@@ -1,3 +1,7 @@
+
+if exist c:\FPC goto installed
 curl -fsSL -o fpc-installer.exe "http://downloads.sourceforge.net/project/freepascal/Win32/3.0.0/fpc-3.0.0.i386-win32.exe?r=&ts=1475862864&use_mirror=pilotfiber"
 fpc-installer.exe /verysilent
 dir c:\FPC\3.0.0
+:installed
+
