c66dc6e4c5cad10327e22f91a5cd67b7c3cccde1
markfirmware <markfirmware@users.noreply.github.com>
2016-10-08 18:24:34 -0400

    ultibo compile

--- build.cmd
+++ build.cmd
@@ -2,3 +2,5 @@ set PATH=c:\FPC\3.0.0\bin\i386-Win32;%PATH%
 fpc appveyortest.lpr
 fpc -FuFPTest appveyortestcheck.lpr
 appveyortestcheck
+
+C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc -B -Tultibo -Parm -CpARMV7A -WpRPI3B @C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\RPI3.CFG -O2 appveyortest.lpr
