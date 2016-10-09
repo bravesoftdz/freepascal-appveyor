dd50be4ae9a3004e810a5e37264554756c3c54a4
markfirmware <markfirmware@users.noreply.github.com>
2016-10-08 21:58:45 -0400

    ultibo x86 unit tests

--- build.cmd
+++ build.cmd
@@ -1,6 +1,11 @@
-set PATH=c:\FPC\3.0.0\bin\i386-Win32;%PATH%
-fpc appveyortest.lpr
-fpc -FuFPTest appveyortestcheck.lpr
+
+rem set PATH=c:\FPC\3.0.0\bin\i386-Win32;%PATH%
+rem fpc appveyortest.lpr
+rem fpc -FuFPTest appveyortestcheck.lpr
+rem appveyortestcheck
+
+C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc appveyortest.lpr
+C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc appveyortestcheck.lpr
 appveyortestcheck
 
 C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc -B -Tultibo -Parm -CpARMV7A -WpRPI3B @C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\RPI3.CFG -O2 appveyortest.lpr
