713fe428a3f6cf5de8b532f27adccb756c257db5
Mark K <markfirmware@users.noreply.github.com>
2016-10-09 01:22:22 -0400

    use src/ folder

--- build.cmd
+++ build.cmd
@@ -1,8 +1,12 @@
 
+cd src
+
 rem c:\FPC\3.0.0\bin\i386-Win32\fpc appveyortest.lpr
 
 del *.exe *.o *.ppu
-c:\FPC\3.0.0\bin\i386-Win32\fpc -FuFPTest appveyortestcheck.lpr
+c:\FPC\3.0.0\bin\i386-Win32\fpc ^
+ -FuFPTest ^
+ appveyortestcheck.lpr
 appveyortestcheck
 
 del *.exe *.o *.ppu
@@ -39,4 +43,12 @@ C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc ^
 appveyortestcheck
 
 del *.exe *.o *.ppu
-C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc -B -Tultibo -Parm -CpARMV7A -WpRPI3B @C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\RPI3.CFG -O2 appveyortest.lpr
+C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc ^
+ -B ^
+ -Tultibo ^
+ -Parm ^
+ -CpARMV7A ^
+ -WpRPI3B ^
+ @C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\RPI3.CFG ^
+ -O2 ^
+ appveyortest.lpr
