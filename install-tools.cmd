763f6d5225a31c078f9e378ca05cc69198e9700b
Mark K <markfirmware@users.noreply.github.com>
2016-10-09 21:03:23 -0400

    .

--- install-tools.cmd
+++ install-tools.cmd
@@ -1,6 +1,4 @@
 
-appveyor AddMessage "installing tools
-"
 if exist c:\FPC goto installed_fpc
 curl -fsSL -o fpc-installer.exe "http://downloads.sourceforge.net/project/freepascal/Win32/3.0.0/fpc-3.0.0.i386-win32.exe?r=&ts=1475862864&use_mirror=pilotfiber"
 fpc-installer.exe /verysilent
