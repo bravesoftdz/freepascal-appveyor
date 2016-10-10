40e0a00f3dd5e716aea7d316035406f42368e36a
Mark K <markfirmware@users.noreply.github.com>
2016-10-10 03:42:47 -0400

    reduce cached installations

--- install-tools.cmd
+++ install-tools.cmd
@@ -3,6 +3,9 @@ if exist c:\FPC goto installed_fpc
 appveyor message "installing fpc"
 curl -fsSL -o fpc-installer.exe "http://downloads.sourceforge.net/project/freepascal/Win32/3.0.0/fpc-3.0.0.i386-win32.exe?r=&ts=1475862864&use_mirror=pilotfiber"
 fpc-installer.exe /verysilent
+rd /s /q c:\Ultibo\Core\demo
+rd /s /q c:\Ultibo\Core\doc
+rd /s /q c:\Ultibo\Core\examples
 dir c:\FPC
 :installed_fpc
 
@@ -10,6 +13,10 @@ if exist c:\Ultibo goto installed_ultibo
 appveyor message "installing ultibo"
 curl -fsSL -o ultibo-installer.exe https://github.com/ultibohub/Core/releases/download/1.2.009/Ultibo-Core-1.2.009-Cucumber.exe
 ultibo-installer /verysilent
+rd /s /q c:\Ultibo\Core\docs
+rd /s /q c:\Ultibo\Core\examples
+rd /s /q c:\Ultibo\Core\firmware
+rd /s /q c:\Ultibo\Core\languages
 dir c:\Ultibo
 curl -fsSL -o ultibo-rtl-update.zip https://github.com/ultibohub/Core/archive/master.zip
 dir *.zip
