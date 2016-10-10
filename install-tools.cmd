fed2ca8675e56b5530b9a2d0b8dfcd18c2ba45f7
Mark K <markfirmware@users.noreply.github.com>
2016-10-10 01:56:56 -0400

    update ultibo rtl

--- install-tools.cmd
+++ install-tools.cmd
@@ -1,11 +1,12 @@
 
+echo on
+goto :installed_fpc
+
 if exist c:\FPC goto installed_fpc
 curl -fsSL -o fpc-installer.exe "http://downloads.sourceforge.net/project/freepascal/Win32/3.0.0/fpc-3.0.0.i386-win32.exe?r=&ts=1475862864&use_mirror=pilotfiber"
 fpc-installer.exe /verysilent
 dir c:\FPC
 :installed_fpc
-
-echo on
 
 if exist c:\Ultibo goto installed_ultibo
 curl -fsSL -o ultibo-installer.exe https://github.com/ultibohub/Core/releases/download/1.2.009/Ultibo-Core-1.2.009-Cucumber.exe
@@ -22,7 +23,6 @@ rem dir c:\Ultibo\Core\fpc\3.1.1\source\rtl\ultibo
 :installed_ultibo
 
 exit 1
-echo off
 
 if exist bootfiles goto installed_bootfiles
 mkdir bootfiles
